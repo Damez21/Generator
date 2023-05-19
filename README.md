c89b16158991bacfd116144f4a04af25f3e4613e
Damiem Mclaurin <damiemmclaurin@Damiems-MacBook-Pro.local>
2023-05-18 23:25:45 -0400

    added readme directions and edited index.js.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Generator
+# Title: Generator
+
+# Description: This README Genterator, is a functional project that asks questions and 
+
+# Installation: You can find my Generator Project @ https://github.com/Damez21/Generator
+
+# How To Use: Click the project, next you will open intergrated terminal. After that, you will proceed to type node index.js and you will be prompted a series of questions that you have to answer in order to proceed to the next. Once that is finished you will have a finished README.md file that is assosciated with your github username.
+
+# Resources- N/A.
+
+## License: This project is licensed under the [Damez21] License - see the LICENSE.md file for details
+
+# Screen Shots: ![pic](
--- potential-enigma-main/Develop/index.js
+++ potential-enigma-main/Develop/index.js
@@ -7,15 +7,63 @@ const generateMarkDown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [
     {
+        type: "input",
+        name: "title",
+        message: "Name Your Project.",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe In Detail The Purpose and functions of this Project."
+    },
+    {
+        type: "input",
+        name: "table of contents",
+        message: "Table of Contents.",
+    },
+    {
+        type: "input",
+        
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
 
     },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.pwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
 function init() {}
-
+inquirer.prompt(questions).then((responses) => {
+    console.log("");
+    writeToFile("./README.md", generateMarkDown({...responses}));
+})
 // Function call to initialize app
 init();
